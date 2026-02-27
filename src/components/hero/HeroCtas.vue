3126f00a108eace3e94098d76087a517808417b6
oriioll <oriolplazas2@gmail.com>
2026-02-27 23:52:06 +0100

    feat: add copy feature for the email

--- src/components/hero/HeroCtas.vue
+++ src/components/hero/HeroCtas.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 //Importing i18n objects
 import { useI18n } from 'vue-i18n';
 const { t, locale } = useI18n();
+
+//Handling email copy to clipboard
+const EMAIL: string = 'oriolplazas2@gmail.com';
+let copied = ref(false)
+
+/**
+ * Function that copies a text to the clipboard and changes a reactive variable to change the svg for 2 seconds
+ * @param txt the txt you want to copy
+ * @author Oriol Plazas León
+ * @since 27/02/2026
+ * @see navigator.clipboard.writeText()
+ * @see changeSvg()
+ */
+function cpText(txt: string): void {
+    navigator.clipboard.writeText(txt).then(changeSvg);
+}
+
+/**
+ * Function that changes a reactive variable to change the svg for 2 seconds
+ * @author Oriol Plazas León
+ * @since 27/02/2026
+ * @see cpText()
+ */
+const changeSvg = (): void => {
+    copied.value = true;
+    setTimeout(() => {
+        copied.value = false;
+    }, 2000);
+}
+
 /**
  * Arrow function that opens a link in a new tab
  * @param url the url you want to open in a new tab
@@ -9,9 +40,11 @@ const { t, locale } = useI18n();
  * @since 27/02/2026
  * @see window()
  */
-const openLink = (url: string) => {
+const openLink = (url: string): void => {
     window.open(url, '_blank');
 }
+
+
 </script>
 <template>
     <section class="ctas">
@@ -29,12 +62,29 @@ const openLink = (url: string) => {
                 d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
                 transform="scale(64)" fill="#ffff" />
         </svg>
-        
+
         <Transition name="fade" mode="out-in">
             <a class="openCv" :key="locale" href="/CV_Oriol_Plazas.pdf" target="_blank">{{ t('hero.openCv') }}</a>
         </Transition>
 
-        <p>oriolplazas2@gmail.com</p>
+        <div class="mail">
+            <p>{{ EMAIL }}</p>
+            <!--Copy to clipboard SVG - Only if not copied -->
+            <svg v-if="!copied" @click="cpText(EMAIL)" class="cpSvg" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24">
+                <g fill="none" stroke="var(--accent-ui)" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="2">
+                    <path
+                        d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z" />
+                    <path d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1" />
+                </g>
+            </svg>
+            <svg v-else class="cpSvg" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"
+                fill="none" stroke="var(--accent-ui)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M5 12l5 5l10 -10" />
+            </svg>
+        </div>
     </section>
 </template>
 <style scoped>
@@ -45,15 +95,18 @@ const openLink = (url: string) => {
     align-items: center;
     gap: 2rem;
 }
+
 .ctas svg {
     width: 2rem;
     height: 2rem;
     cursor: pointer;
     transition: all .25s ease-out;
 }
+
 .ctas svg:hover {
     transform: translateY(-2px);
 }
+
 .openCv {
     text-decoration: none;
     padding: .8rem 1.5rem;
@@ -68,6 +121,25 @@ const openLink = (url: string) => {
 .openCv:hover {
     filter: brightness(1.2);
     transform: translateY(-2px);
+}
+
+.mail {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: .75rem;
+    border: solid 1px var(--accent-ui);
+    border-radius: 10px;
+    background-color: rgba(236, 143, 92, 0.1);
+    padding: .8rem 1.5rem;
+    color: var(--accent-ui);
+    font-weight: 600;
+}
+
+.mail .cpSvg {
+    width: 1.25rem;
+    height: 1.25rem;
 }
 
 /*Animations for when changing lang */
